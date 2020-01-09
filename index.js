25321c3c7a5e1450ce2a6e0c2e82b46886f21885
benjacoblee <benjacoblee@gmail.com>
2020-01-09 13:26:22 +0800

    mvp working

---
+++ index.js
@@ -0,0 +1,14 @@
+if (process.argv[2] === "hex") {
+  console.log(hexToRgb(process.argv[3]));
+}
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      }
+    : null;
+}
