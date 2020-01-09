5972fad4a7ba64631f18bcb61b93a50ca7cefbd7
benjacoblee <benjacoblee@gmail.com>
2020-01-09 17:34:14 +0800

    refactor

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const hexToRgb = require("./hexToRgb");
 const rgbToHsl = require("./rgbToHsl");
 const hexToHsl = require("./hexToHsl");
 const harmonize = require("./harmonize");
+const r = parseInt(process.argv[3]);
+const g = parseInt(process.argv[4]);
+const b = parseInt(process.argv[5]);
 
 if (process.argv[2] === "hex") {
   for (let i = 3; i < process.argv.length; i++) {
     console.log("Converted color: ", hexToRgb(process.argv[i]));
   }
 } else if (process.argv[2] === "rgb") {
-  let r = parseInt(process.argv[3]);
-  let g = parseInt(process.argv[4]);
-  let b = parseInt(process.argv[5]);
-  console.log(rgbToHex(r, g, b));
-  console.log(rgbToHsl(r, g, b));
+  console.log("Hex: ",rgbToHex(r, g, b));
+  console.log("HSL: ",rgbToHsl(r, g, b));
 } else if (process.argv[2] === "color") {
-  let r = parseInt(process.argv[3]);
-  let g = parseInt(process.argv[4]);
-  let b = parseInt(process.argv[5]);
   let myOutput = process.argv[6];
   console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);
 } else if (process.argv[2] === "harmony") {
